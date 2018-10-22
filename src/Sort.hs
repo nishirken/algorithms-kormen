d9d03e190d6d82a82314242e553d7372ce65ff65
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2018-10-23 00:00:56 +0500

    add merge sort

--- src/Sort.hs
+++ src/Sort.hs
@@ -1,9 +1,24 @@
-module Sort (insertionSort) where
+module Sort (insertionSort, mergeSort) where
 
 insertionSort :: Ord a => [a] -> [a]
-insertionSort [] = []
-insertionSort xs = foldr insert [] xs
+insertionSort = foldr insert []
     where
         insert :: Ord a => a -> [a] -> [a]
         insert x [] = [x]
         insert x acc@(y:ys) = if x > y then y : insert x ys else x : acc
+
+mergeSort :: Ord a => [a] -> [a]
+mergeSort xs = _sort xs $ length xs
+    where
+        merge :: Ord a => [a] -> [a] -> [a]
+        merge [] x = x
+        merge x [] = x
+        merge left@(x:xs) right@(y:ys) = if x < y then x : merge xs right else y : merge left ys
+        _sort :: Ord a => [a] -> Int -> [a]
+        _sort [x] _ = [x]
+        _sort [] _ = []
+        _sort xs len =
+            let
+                half = len `div` 2
+                (left, right) = splitAt half xs in
+                merge (_sort left half) (_sort right $ half + 1)
--- test/SortSpec.hs
+++ test/SortSpec.hs
@@ -3,9 +3,10 @@ module SortSpec where
 import Test.Hspec (describe, it, SpecWith)
 import Test.QuickCheck (property)
 import Data.List (sort)
-import Sort (insertionSort)
+import Sort (insertionSort, mergeSort)
 
 sortSpec :: SpecWith ()
 sortSpec =
     describe "Sort" $ do
-        it "Insertion" $ property $ \x -> insertionSort (x :: [Int]) == sort x 
+        it "Insertion" $ property $ \x -> insertionSort (x :: [Int]) == sort x
+        it "Merge" $ property $ \x -> mergeSort (x :: [Int]) == sort x
