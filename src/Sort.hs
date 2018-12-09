3376a55f08359950a9f20bd747637379e79ddf3a
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2018-12-09 23:57:33 +0500

    add heap sort

--- src/Sort.hs
+++ src/Sort.hs
@@ -1,4 +1,12 @@
-module Sort (bubbleSort, insertionSort, mergeSort) where
+module Sort (
+  bubbleSort
+  , insertionSort
+  , mergeSort
+  , maxHeapify
+  , buildMaxHeap
+  , heapSort
+  , swap
+  ) where
 
 insertionSort :: Ord a => [a] -> [a]
 insertionSort = foldr insert []
@@ -29,3 +37,60 @@ bubbleSort xs = outer xs 0
         outer ys counter = if counter == length ys then ys else outer (inner ys) (counter + 1)
         inner [z] = [z]
         inner (z:z':zs) = if z > z' then z' : inner (z:zs) else z : inner (z':zs)
+
+-- Heap sort
+
+type Index = Int
+
+left :: Index -> Index
+left i = 2 * i + 1
+
+right :: Index -> Index
+right i = 2 * i + 2
+
+swap :: Eq a => [a] -> Index -> Index -> [a]
+swap [] _ _ = []
+swap [x] _ _ = [x]
+swap xs index newIndex =
+  if correct index && correct newIndex && index < newIndex
+    then left ++ [newItem] ++ middle ++ [item] ++ right
+    else error $ "Incorrect index in replace" ++ show (index, newIndex)
+      where
+        correct i = i < length xs && i >= 0
+        item = xs !! index
+        newItem = xs !! newIndex
+        left = take index xs
+        middle = take (newIndex - index - 1) (drop (index + 1) xs)
+        right = drop (newIndex + 1) xs
+
+maxHeapify :: Ord a => [a] -> Index -> Int -> [a]
+maxHeapify heap i heapSize =
+  if i == largest
+    then heap
+    else maxHeapify (swap heap i largest) largest heapSize
+      where
+        l = left i
+        r = right i
+        getLargest :: Index -> Index -> Index
+        getLargest childIndex currentIndex =
+          if childIndex <= heapSize && ((heap !! childIndex) > (heap !! currentIndex))
+            then childIndex
+            else currentIndex
+        largest = getLargest r $ getLargest l i
+
+buildMaxHeap :: Ord a => [a] -> [a]
+buildMaxHeap xs = iter xs (div (length xs) 2)
+  where
+    iter :: Ord a => [a] -> Int -> [a]
+    iter ys 0 = ys
+    iter ys len =
+      let next = len - 1 in iter (maxHeapify ys next (length xs - 1)) next
+
+heapSort :: Ord a => [a] -> [a]
+heapSort [] = []
+heapSort xs = iter heap (length xs - 1)
+  where
+    heap = buildMaxHeap xs
+    iter :: Ord a => [a] -> Int -> [a]
+    iter acc 0 = acc
+    iter acc i = iter (maxHeapify (swap acc 0 i) 0 (i - 1)) (i - 1)
--- test/SortSpec.hs
+++ test/SortSpec.hs
@@ -1,13 +1,41 @@
 module SortSpec where
 
-import Test.Hspec (describe, it, SpecWith)
+import Test.Hspec (context, describe, it, SpecWith)
 import Test.QuickCheck (property)
 import Data.List (sort)
-import Sort (bubbleSort, insertionSort, mergeSort)
+import Sort (
+  bubbleSort
+  , insertionSort
+  , mergeSort
+  , heapSort
+  , maxHeapify
+  , swap
+  , buildMaxHeap
+  , heapSort
+  )
 
 sortSpec :: SpecWith ()
 sortSpec =
-    describe "Sort" $ do
-        it "Insertion" $ property $ \x -> insertionSort (x :: [Int]) == sort x
-        it "Merge" $ property $ \x -> mergeSort (x :: [Int]) == sort x
-        it "Bubble" $ property $ \x -> bubbleSort (x :: [Int]) == sort x
+  describe "Sort" $ do
+    context "swap fn" $ do
+      it "works with empty" $ swap ([] :: [Int]) 0 0 `shouldBe` []
+      it "works with single" $ swap [1] 0 0 `shouldBe` [1]
+      it "works" $ swap [1, 4, 5, 6, 7] 1 3 `shouldBe` [1, 6, 5, 4, 7]
+
+    it "Insertion" $ property $ \x -> insertionSort (x :: [Int]) `shouldBe` sort x
+    it "Merge" $ property $ \x -> mergeSort (x :: [Int]) `shouldBe` sort x
+    it "Bubble" $ property $ \x -> bubbleSort (x :: [Int]) `shouldBe` sort x
+    context "Heap sort" $ do
+      it "maxHeapify" $
+        let
+          expected = [27, 17, 10, 16, 13, 9, 1, 5, 7, 12, 4, 8, 3, 0]
+          testList = [27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0]
+          in (maxHeapify testList 2 (length testList - 1)) `shouldBe` expected
+
+      it "buildMaxHeap" $
+        let
+          expected = [84, 22, 19, 10, 3, 17, 6, 5, 9]
+          testList = [5, 3, 17, 10, 84, 19, 6, 22, 9]
+          in buildMaxHeap testList `shouldBe` expected
+
+      it "Heap sort" $ property $ \x -> heapSort (x :: [Int]) `shouldBe` sort x
