f9870e6438102bf7af14c35a98ed8acba5d0221e
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2018-12-18 09:32:39 +0500

    proceed bench

--- benchmarks/Benchmarks.hs
+++ benchmarks/Benchmarks.hs
@@ -1,5 +1,5 @@
 import Sort (insertionSort)
-import Criterion.Main (defaultMainWith, defaultConfig, bgroup, bench, whnf, Benchmark, env)
+import Criterion.Main (defaultMainWith, defaultConfig, bgroup, bench, nf, Benchmark, env)
 import Criterion.Types (reportFile)
 import Test.QuickCheck (generate, vectorOf, arbitrary, Gen)
 
@@ -13,13 +13,13 @@ benchCases sortFn benchName = bgroup benchName $ map makeOneBench benchList
     makeOneBench :: Int -> Benchmark
     makeOneBench listSize = env
       (arbitraryList listSize)
-      (\ ~xs -> bench (show listSize) $ whnf sortFn xs)
+      (\ ~xs -> bench (show listSize) $ nf sortFn xs)
     arbitraryList :: Int -> IO [Int]
     arbitraryList n = generate $ vectorOf n (arbitrary :: Gen Int)
 
 main :: IO ()
 main = defaultMainWith
-  (defaultConfig { reportFile = Just "benchmarks/sort-bench.html" })
+  (defaultConfig { reportFile = Just "benchmarks/sort-bench-nf.html" })
   [
     benchCases insertionSort "insertion sort"
   ]
