cff525700d1b23ed72cde4b5b21f24d9a8551863
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2020-01-02 15:37:11 +0300

    add queue

--- test/DataStructuresSpec.hs
+++ test/DataStructuresSpec.hs
@@ -3,9 +3,10 @@ module DataStructuresSpec where
 import Test.Hspec (context, describe, it, Spec, shouldBe, shouldThrow, Selector)
 import Test.QuickCheck (Property, property)
 import qualified DataStructures.Stack as S
+import qualified DataStructures.Queue as Q
 
 dataStructuresSpec :: Spec
-dataStructuresSpec = describe "StackSpec" $ do
+dataStructuresSpec = describe "DataStructuresSpec" $ do
   context "Stack" $ do
     let
       shownEmpty :: S.Stack [Int]
@@ -20,3 +21,18 @@ dataStructuresSpec = describe "StackSpec" $ do
     it "isEmpty with not empty" $ S.isEmpty (S.fromList [1]) `shouldBe` False
     it "size with empty" $ S.size shownEmpty `shouldBe` 0
     it "size with not empty" $ S.size (S.fromList [1]) `shouldBe` 1
+
+  context "Queue" $ do
+    let
+      shownEmpty :: Q.Queue [Int]
+      shownEmpty = Q.empty
+    it "creates from list" $ Q.fromList [1, 2, 3] `shouldBe` Q.Queue 3 [1, 2, 3]
+    it "creates new" $ shownEmpty `shouldBe` Q.fromList []
+    it "enqueue" $ Q.enqueue (Q.fromList [1, 2]) 3 `shouldBe` Q.fromList [1, 2, 3]
+    it "enqueue with empty" $ (Q.enqueue Q.empty 3) `shouldBe` Q.fromList [3]
+    it "dequeue" $ Q.dequeue (Q.fromList [1, 2]) `shouldBe` Just (Q.fromList [2], 1)
+    it "dequeue with empty" $ Q.dequeue shownEmpty `shouldBe` Nothing
+    it "isEmpty with empty" $ Q.isEmpty shownEmpty `shouldBe` True
+    it "isEmpty with not empty" $ Q.isEmpty (Q.fromList [1]) `shouldBe` False
+    it "size with empty" $ Q.size shownEmpty `shouldBe` 0
+    it "size with not empty" $ Q.size (Q.fromList [1]) `shouldBe` 1
